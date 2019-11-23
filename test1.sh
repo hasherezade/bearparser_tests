b8a936ea1829dd390b33832f3868f3adc202b7ef
hasherezade <hasherezade@gmail.com>
2019-11-23 22:32:49 +0100

    Check if the output file was generated

--- test1.sh
+++ test1.sh
@@ -7,14 +7,18 @@ FILE="$BINARY_PATH".dll
 
 echo "Starting test: " "$BINARY_PATH" ":" "$TEST_NAME"
 
-if test -f "$FILE"; then
-	echo "$FILE exist"
-else
+if ! test -f "$FILE"; then
 	echo "$FILE NOT found!"
 	exit 2
 fi
 
 $COMMANDER_PATH/bearcommander "$FILE" < "$BINARY_PATH"/"$TEST_NAME"_in.txt > "$BINARY_PATH"/"$TEST_NAME"_test.txt
+
+if ! test -f "$BINARY_PATH"/"$TEST_NAME"_test.txt; then
+	echo "Output file NOT found!"
+	exit 3
+fi
+
 DIFF_OUT=$(diff "$BINARY_PATH"/"$TEST_NAME"_out.txt "$BINARY_PATH"/"$TEST_NAME"_test.txt)
 
 if [[ "$DIFF_OUT" == "" ]]; then
